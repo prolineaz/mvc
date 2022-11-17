3bf441cf458775a11152703aa04967a19d1ead79
Prolineaz <prolineaz@gmail.com>
2022-11-17 11:05:40 +0400

    Update comment

--- public/index.php
+++ public/index.php
@@ -4,14 +4,14 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new Framework\Application();
 
 /**
- * Route with callback function
+ * Routing with callback function
  */
 $app->router->get('/', function () {
     return (new App\Controller\Controller)->view('home.html.twig', ['testvar' => 'Lorem ipsum — классический текст-«рыба». Является искажённым отрывком из философского трактата Марка Туллия Цицерона «О пределах добра и зла», написанного в 45 году до н. э. на латинском языке, обнаружение сходства приписывается Ричарду Макклинтоку.']);
 });
 
 /**
- * Route with class
+ * Routing with class
  */
 $app->router->get('/front', 'App\Controller\TestController@front');
 $app->router->get('/back', 'App\Controller\TestController@back');
